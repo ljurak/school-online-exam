d20f4170bd9b7f4a858b76d49bddc66b7cc4fa3d
ljurak <lucas.jurak@gmail.com>
2020-03-17 23:28:40 +0100

    Initialize project

---
+++ src/main/java/com/schoolonline/app/SchoolExamOnlineApplication.java
@@ -0,0 +1,12 @@
+package com.schoolonline.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolExamOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolExamOnlineApplication.class, args);
+	}
+}
---
+++ src/test/java/com/schoolonline/app/SchoolExamOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schoolonline.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolExamOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
