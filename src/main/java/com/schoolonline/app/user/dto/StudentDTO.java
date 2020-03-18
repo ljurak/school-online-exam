a16e56e648997e13e8595630e27e484aa1e6e025
ljurak <lucas.jurak@gmail.com>
2020-03-18 06:21:19 +0100

    Add StudentDTO class

---
+++ src/main/java/com/schoolonline/app/user/dto/StudentDTO.java
@@ -0,0 +1,32 @@
+package com.schoolonline.app.user.dto;
+
+public class StudentDTO {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public StudentDTO(Long id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
