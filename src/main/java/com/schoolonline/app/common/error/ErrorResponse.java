75564ebfce742ac3de616257e7bfd75de15fb429
ljurak <lucas.jurak@gmail.com>
2020-03-18 18:49:16 +0100

    Add error message object

---
+++ src/main/java/com/schoolonline/app/common/error/ErrorResponse.java
@@ -0,0 +1,23 @@
+package com.schoolonline.app.common.error;
+
+import java.time.LocalDateTime;
+
+public class ErrorResponse {
+
+    private String message;
+
+    private LocalDateTime timestamp;
+
+    public ErrorResponse(String message) {
+        this.message = message;
+        this.timestamp = LocalDateTime.now();
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+}
