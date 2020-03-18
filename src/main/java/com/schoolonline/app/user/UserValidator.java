39b4bc4cdd2e2b8bfce19675529c8d48f2a4b941
ljurak <lucas.jurak@gmail.com>
2020-03-18 17:09:38 +0100

    Refactor method validateUser

--- src/main/java/com/schoolonline/app/user/UserValidator.java
+++ src/main/java/com/schoolonline/app/user/UserValidator.java
@@ -17,26 +17,51 @@ class UserValidator {
     }
 
     Either<UserError, NewUserDTO> validateUser(NewUserDTO newUserDTO) {
+        return validateFirstName(newUserDTO)
+                .flatMap(this::validateLastName)
+                .flatMap(this::validateEmail)
+                .flatMap(this::validatePassword);
+    }
+
+    private Either<UserError, NewUserDTO> validateFirstName(NewUserDTO newUserDTO) {
         if (validator.isBlank(newUserDTO.getFirstName())) {
             return Either.left(UserError.MISSING_FIRSTNAME);
         } else if (!validator.hasMaxLength(newUserDTO.getFirstName(), 30)) {
             return Either.left(UserError.INVALID_FIRSTNAME);
-        } else if (validator.isBlank(newUserDTO.getLastName())) {
+        }
+
+        return Either.right(newUserDTO);
+    }
+
+    private Either<UserError, NewUserDTO> validateLastName(NewUserDTO newUserDTO) {
+        if (validator.isBlank(newUserDTO.getLastName())) {
             return Either.left(UserError.MISSING_LASTNAME);
         } else if (!validator.hasMaxLength(newUserDTO.getLastName(), 30)) {
             return Either.left(UserError.INVALID_LASTNAME);
-        } else if (validator.isBlank(newUserDTO.getEmail())) {
+        }
+
+        return Either.right(newUserDTO);
+    }
+
+    private Either<UserError, NewUserDTO> validateEmail(NewUserDTO newUserDTO) {
+        if (validator.isBlank(newUserDTO.getEmail())) {
             return Either.left(UserError.MISSING_EMAIL);
         } else if (!validator.isValidEmail(newUserDTO.getEmail())) {
             return Either.left(UserError.INVALID_EMAIL);
-        } else if (validator.isBlank(newUserDTO.getPassword())) {
-            return Either.left(UserError.MISSING_PASSWORD);
-        } else if (!validator.hasLengthBeetwen(newUserDTO.getPassword(), 8, 20)) {
-            return Either.left(UserError.INVALID_PASSWORD);
         }
 
         if (userRepository.existsByEmail(newUserDTO.getEmail())) {
             return Either.left(UserError.DUPLICATED_EMAIL);
+        }
+
+        return Either.right(newUserDTO);
+    }
+
+    private Either<UserError, NewUserDTO> validatePassword(NewUserDTO newUserDTO) {
+        if (validator.isBlank(newUserDTO.getPassword())) {
+            return Either.left(UserError.MISSING_PASSWORD);
+        } else if (!validator.hasLengthBeetwen(newUserDTO.getPassword(), 8, 20)) {
+            return Either.left(UserError.INVALID_PASSWORD);
         }
 
         return Either.right(newUserDTO);
