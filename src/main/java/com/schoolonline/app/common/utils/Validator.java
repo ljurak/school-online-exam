44b2589d88b1a4e651645fca7912076963d356de
ljurak <lucas.jurak@gmail.com>
2020-03-19 05:15:30 +0100

    Add validation methods for dates

--- src/main/java/com/schoolonline/app/common/utils/Validator.java
+++ src/main/java/com/schoolonline/app/common/utils/Validator.java
@@ -1,11 +1,16 @@
 package com.schoolonline.app.common.utils;
 
+import java.time.LocalDate;
 import java.util.regex.Pattern;
 
 public class Validator {
 
     private Pattern emailPattern =
             Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$",Pattern.CASE_INSENSITIVE);
+
+    public boolean isNull(Object o) {
+        return o == null;
+    }
 
     public boolean isEmpty(String s) {
         return s == null || s.isEmpty();
@@ -29,5 +34,13 @@ public class Validator {
 
     public boolean isValidEmail(String email) {
         return emailPattern.matcher(email).matches();
+    }
+
+    public boolean isPresentOrFutureDate(LocalDate date) {
+        return date.compareTo(LocalDate.now()) >= 0;
+    }
+
+    public boolean isEarlierDate(LocalDate start, LocalDate end) {
+        return start.compareTo(end) < 0;
     }
 }
