6d6faeb0c09010284d2c594f7274809a331b0ddf
ljurak <lucas.jurak@gmail.com>
2020-03-19 05:21:14 +0100

    Add validator for new courses

---
+++ src/main/java/com/schoolonline/app/course/CourseValidator.java
@@ -0,0 +1,60 @@
+package com.schoolonline.app.course;
+
+import com.schoolonline.app.common.utils.Validator;
+import com.schoolonline.app.course.dto.NewCourseDTO;
+import com.schoolonline.app.course.error.CourseError;
+import io.vavr.control.Either;
+
+class CourseValidator {
+
+    private Validator validator;
+
+    public CourseValidator(Validator validator) {
+        this.validator = validator;
+    }
+
+    Either<CourseError, NewCourseDTO> validateCourse(NewCourseDTO newCourseDTO) {
+        return validateName(newCourseDTO)
+                .flatMap(this::validateStartDate)
+                .flatMap(this::validateEndDate)
+                .flatMap(this::validateTeacherId);
+    }
+
+    private Either<CourseError, NewCourseDTO> validateName(NewCourseDTO newCourseDTO) {
+        if (validator.isBlank(newCourseDTO.getName())) {
+            return Either.left(CourseError.MISSING_NAME);
+        } else if (!validator.hasMaxLength(newCourseDTO.getName(), 100)) {
+            return Either.left(CourseError.INVALID_NAME);
+        }
+
+        return Either.right(newCourseDTO);
+    }
+
+    private Either<CourseError, NewCourseDTO> validateStartDate(NewCourseDTO newCourseDTO) {
+        if (validator.isNull(newCourseDTO.getStartDate())) {
+            return Either.left(CourseError.MISSING_START_DATE);
+        } else if (!validator.isPresentOrFutureDate(newCourseDTO.getStartDate())) {
+            return Either.left(CourseError.INVALID_START_DATE);
+        }
+
+        return Either.right(newCourseDTO);
+    }
+
+    private Either<CourseError, NewCourseDTO> validateEndDate(NewCourseDTO newCourseDTO) {
+        if (validator.isNull(newCourseDTO.getEndDate())) {
+            return Either.left(CourseError.MISSING_END_DATE);
+        } else if (!validator.isEarlierDate(newCourseDTO.getStartDate(), newCourseDTO.getEndDate())) {
+            return Either.left(CourseError.INVALID_END_DATE);
+        }
+
+        return Either.right(newCourseDTO);
+    }
+
+    private Either<CourseError, NewCourseDTO> validateTeacherId(NewCourseDTO newCourseDTO) {
+        if (validator.isNull(newCourseDTO.getTeacherId())) {
+            return Either.left(CourseError.MISSING_TEACHER_ID);
+        }
+
+        return Either.right(newCourseDTO);
+    }
+}
