7e5b562058434f19171c4591c8fd90563953416f
ljurak <lucas.jurak@gmail.com>
2020-03-20 23:31:42 +0100

    Add query method findStudentById

--- src/main/java/com/schoolonline/app/user/StudentRepository.java
+++ src/main/java/com/schoolonline/app/user/StudentRepository.java
@@ -1,8 +1,11 @@
 package com.schoolonline.app.user;
 
+import io.vavr.control.Option;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 interface StudentRepository extends CrudRepository<Student, Long> {
+
+    Option<Student> findStudentById(Long id);
 }
