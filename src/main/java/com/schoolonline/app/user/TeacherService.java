345ea5c994c49de70b8681c9408068451003195c
ljurak <lucas.jurak@gmail.com>
2020-03-20 23:34:33 +0100

    Add method for removing teacher

--- src/main/java/com/schoolonline/app/user/TeacherService.java
+++ src/main/java/com/schoolonline/app/user/TeacherService.java
@@ -4,6 +4,7 @@ import com.schoolonline.app.user.dto.NewUserDTO;
 import com.schoolonline.app.user.dto.TeacherDTO;
 import com.schoolonline.app.user.error.UserError;
 import io.vavr.control.Either;
+import io.vavr.control.Option;
 
 class TeacherService {
 
@@ -11,14 +12,29 @@ class TeacherService {
 
     private TeacherRepository teacherRepository;
 
-    TeacherService(UserFactory userFactory, TeacherRepository teacherRepository) {
+    private UserRepository userRepository;
+
+    TeacherService(UserFactory userFactory, TeacherRepository teacherRepository, UserRepository userRepository) {
         this.userFactory = userFactory;
         this.teacherRepository = teacherRepository;
+        this.userRepository = userRepository;
     }
 
     Either<UserError, TeacherDTO> addTeacher(NewUserDTO newUserDTO) {
         return userFactory
                 .addTeacher(newUserDTO)
                 .map(Teacher::toDTO);
+    }
+
+    Option<Teacher> findById(Long id) {
+        return teacherRepository.findTeacherById(id);
+    }
+
+    void removeTeacherById(Long id) {
+        teacherRepository.findTeacherById(id)
+                .peek(teacher -> {
+                   teacherRepository.delete(teacher);
+                   userRepository.delete(teacher.getUser());
+                });
     }
 }
