96e40775096a5a6565d287bb80646d137c184386
ljurak <lucas.jurak@gmail.com>
2020-03-20 23:38:36 +0100

    Add factory for courses

---
+++ src/main/java/com/schoolonline/app/course/CourseFactory.java
@@ -0,0 +1,26 @@
+package com.schoolonline.app.course;
+
+import com.schoolonline.app.course.dto.NewCourseDTO;
+import com.schoolonline.app.course.error.CourseError;
+import io.vavr.control.Either;
+
+class CourseFactory {
+
+    private CourseValidator courseValidator;
+
+    private CourseRepository courseRepository;
+
+    CourseFactory(CourseValidator courseValidator, CourseRepository courseRepository) {
+        this.courseValidator = courseValidator;
+        this.courseRepository = courseRepository;
+    }
+
+    Either<CourseError, Course> addCourse(NewCourseDTO newCourseDTO) {
+        return courseValidator
+                .validateCourse(newCourseDTO)
+                .map(newCourse -> {
+                   Course course = Course.fromDTO(newCourseDTO);
+                   return courseRepository.save(course);
+                });
+    }
+}
