6e128ee07f86197c1406172ddaacba70e9a6388c
ljurak <lucas.jurak@gmail.com>
2020-03-20 23:33:29 +0100

    Add method for removing student

--- src/main/java/com/schoolonline/app/user/StudentService.java
+++ src/main/java/com/schoolonline/app/user/StudentService.java
@@ -4,6 +4,7 @@ import com.schoolonline.app.user.dto.NewUserDTO;
 import com.schoolonline.app.user.dto.StudentDTO;
 import com.schoolonline.app.user.error.UserError;
 import io.vavr.control.Either;
+import io.vavr.control.Option;
 
 class StudentService {
 
@@ -11,14 +12,25 @@ class StudentService {
 
     private StudentRepository studentRepository;
 
-    StudentService(UserFactory userFactory, StudentRepository studentRepository) {
+    private UserRepository userRepository;
+
+    StudentService(UserFactory userFactory, StudentRepository studentRepository, UserRepository userRepository) {
         this.userFactory = userFactory;
         this.studentRepository = studentRepository;
+        this.userRepository = userRepository;
     }
 
     Either<UserError, StudentDTO> addStudent(NewUserDTO newUserDTO) {
         return userFactory
                 .addStudent(newUserDTO)
                 .map(Student::toDTO);
+    }
+
+    void removeStudentById(Long id) {
+        studentRepository.findStudentById(id)
+                .peek(student -> {
+                    studentRepository.delete(student);
+                    userRepository.delete(student.getUser());
+                });
     }
 }
