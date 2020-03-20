94e9d33816c6271ff8d85b8130a23726f8e2d539
ljurak <lucas.jurak@gmail.com>
2020-03-20 23:32:25 +0100

    Add query method findTeacherById

--- src/main/java/com/schoolonline/app/user/TeacherRepository.java
+++ src/main/java/com/schoolonline/app/user/TeacherRepository.java
@@ -1,8 +1,11 @@
 package com.schoolonline.app.user;
 
+import io.vavr.control.Option;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 interface TeacherRepository extends CrudRepository<Teacher, Long> {
+
+    Option<Teacher> findTeacherById(Long id);
 }
