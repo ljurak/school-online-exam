e31fa30acba51fe62e6f31a24291d0617db0c008
ljurak <lucas.jurak@gmail.com>
2020-03-21 23:43:24 +0100

    Add facade for course module

---
+++ src/main/java/com/schoolonline/app/course/CourseFacade.java
@@ -0,0 +1,21 @@
+package com.schoolonline.app.course;
+
+import com.schoolonline.app.course.dto.CourseDTO;
+import com.schoolonline.app.course.dto.NewCourseDTO;
+import com.schoolonline.app.course.error.CourseError;
+import io.vavr.control.Either;
+import org.springframework.transaction.annotation.Transactional;
+
+public class CourseFacade {
+
+    private CourseService courseService;
+
+    public CourseFacade(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @Transactional
+    public Either<CourseError, CourseDTO> addCourse(NewCourseDTO newCourseDTO) {
+        return courseService.addCourse(newCourseDTO);
+    }
+}
