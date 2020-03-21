f9720b075f0d71a1df93f0103e079c4cf2332766
ljurak <lucas.jurak@gmail.com>
2020-03-21 23:57:44 +0100

    Add query for courses by teacher

--- src/main/java/com/schoolonline/app/course/CourseRepository.java
+++ src/main/java/com/schoolonline/app/course/CourseRepository.java
@@ -1,8 +1,11 @@
 package com.schoolonline.app.course;
 
+import io.vavr.collection.List;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 interface CourseRepository extends CrudRepository<Course, Long> {
+
+    List<Course> findCoursesByTeacherId(Long id);
 }
