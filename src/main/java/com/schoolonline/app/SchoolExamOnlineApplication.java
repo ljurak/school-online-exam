5c57e1f88b426731c20ccba1fcb0f613cb0bd689
ljurak <lucas.jurak@gmail.com>
2020-03-22 00:14:10 +0100

    Add VavrModule for Jackson ObjectMapper

--- src/main/java/com/schoolonline/app/SchoolExamOnlineApplication.java
+++ src/main/java/com/schoolonline/app/SchoolExamOnlineApplication.java
@@ -1,12 +1,20 @@
 package com.schoolonline.app;
 
+import com.fasterxml.jackson.databind.Module;
+import io.vavr.jackson.datatype.VavrModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SchoolExamOnlineApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SchoolExamOnlineApplication.class, args);
+	}
+
+	@Bean
+	public Module vavrModule() {
+		return new VavrModule();
 	}
 }
