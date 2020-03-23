cc6605375d14a990dfc32d9c4d9abbc0680b35e5
ljurak <lucas.jurak@gmail.com>
2020-03-23 23:56:06 +0100

    Add endpoint for retrieving courses list by teacher

--- src/main/java/com/schoolonline/app/course/CourseController.java
+++ src/main/java/com/schoolonline/app/course/CourseController.java
@@ -4,9 +4,12 @@ import com.schoolonline.app.common.utils.ResponseProcessor;
 import com.schoolonline.app.course.dto.CourseDTO;
 import com.schoolonline.app.course.dto.NewCourseDTO;
 import com.schoolonline.app.course.error.CourseError;
+import io.vavr.collection.List;
 import io.vavr.control.Either;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,5 +27,11 @@ class CourseController {
     ResponseEntity<?> addCourse(@RequestBody NewCourseDTO newCourseDTO) {
         Either<CourseError, CourseDTO> courseDTO = courseFacade.addCourse(newCourseDTO);
         return ResponseProcessor.processResponse(courseDTO, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/teachers/{teacherId}/courses")
+    ResponseEntity<?> findTeacherCourses(@PathVariable long teacherId) {
+        List<CourseDTO> courses = courseFacade.findCoursesByTeacherId(teacherId);
+        return ResponseEntity.ok(courses);
     }
 }
