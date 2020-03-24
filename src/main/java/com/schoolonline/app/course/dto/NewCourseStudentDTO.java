68d8e41d60b92ce86025980b8974b672f95ba34f
ljurak <lucas.jurak@gmail.com>
2020-03-24 23:44:13 +0100

    Add dto for adding student to course

---
+++ src/main/java/com/schoolonline/app/course/dto/NewCourseStudentDTO.java
@@ -0,0 +1,24 @@
+package com.schoolonline.app.course.dto;
+
+public class NewCourseStudentDTO {
+
+    private Long studentId;
+
+    private Long courseId;
+
+    public Long getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Long studentId) {
+        this.studentId = studentId;
+    }
+
+    public Long getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(Long courseId) {
+        this.courseId = courseId;
+    }
+}
