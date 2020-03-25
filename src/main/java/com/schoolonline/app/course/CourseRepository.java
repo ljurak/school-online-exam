b427b88fdf73f7296713fe85038f2955939d81fc
ljurak <lucas.jurak@gmail.com>
2020-03-25 23:48:50 +0100

    Add query method findCourseById

--- src/main/java/com/schoolonline/app/course/CourseRepository.java
+++ src/main/java/com/schoolonline/app/course/CourseRepository.java
@@ -1,11 +1,14 @@
 package com.schoolonline.app.course;
 
 import io.vavr.collection.List;
+import io.vavr.control.Option;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 interface CourseRepository extends CrudRepository<Course, Long> {
+
+    Option<Course> findCourseById(Long id);
 
     List<Course> findCoursesByTeacherId(Long teacherId);
 }
