51dcb226bb91d188de6061d1ef34b2688e64a86e
ljurak <lucas.jurak@gmail.com>
2020-03-25 23:53:13 +0100

    Add method findCourseById and addStudentToCourse

--- src/main/java/com/schoolonline/app/course/CourseService.java
+++ src/main/java/com/schoolonline/app/course/CourseService.java
@@ -1,10 +1,13 @@
 package com.schoolonline.app.course;
 
 import com.schoolonline.app.course.dto.CourseDTO;
+import com.schoolonline.app.course.dto.CourseStudentDTO;
 import com.schoolonline.app.course.dto.NewCourseDTO;
+import com.schoolonline.app.course.dto.NewCourseStudentDTO;
 import com.schoolonline.app.course.error.CourseError;
 import io.vavr.collection.List;
 import io.vavr.control.Either;
+import io.vavr.control.Option;
 
 class CourseService {
 
@@ -12,9 +15,12 @@ class CourseService {
 
     private CourseRepository courseRepository;
 
-    CourseService(CourseFactory courseFactory, CourseRepository courseRepository) {
+    private CourseStudentRepository courseStudentRepository;
+
+    CourseService(CourseFactory courseFactory, CourseRepository courseRepository, CourseStudentRepository courseStudentRepository) {
         this.courseFactory = courseFactory;
         this.courseRepository = courseRepository;
+        this.courseStudentRepository = courseStudentRepository;
     }
 
     Either<CourseError, CourseDTO> addCourse(NewCourseDTO newCourseDTO) {
@@ -23,9 +29,26 @@ class CourseService {
                 .map(Course::toDTO);
     }
 
+    Option<CourseDTO> findCourseById(Long id) {
+        return courseRepository
+                .findCourseById(id)
+                .map(Course::toDTO);
+    }
+
     List<CourseDTO> findCoursesByTeacherId(Long teacherId) {
         return courseRepository
                 .findCoursesByTeacherId(teacherId)
                 .map(Course::toDTO);
+    }
+
+    Either<CourseError, CourseStudentDTO> addStudentToCourse(NewCourseStudentDTO newCourseStudentDTO) {
+        return courseRepository
+                .findCourseById(newCourseStudentDTO.getCourseId())
+                .toEither(CourseError.COURSE_NOT_FOUND)
+                .map(course -> {
+                    CourseStudent courseStudent = CourseStudent.of(newCourseStudentDTO.getStudentId(), course);
+                    return courseStudentRepository.save(courseStudent);
+                })
+                .map(CourseStudent::toDTO);
     }
 }
