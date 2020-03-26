5bdf15c5f16e9aa2419cb7f301f19d9d35a75b1f
ljurak <lucas.jurak@gmail.com>
2020-03-26 23:29:17 +0100

    Add endpoints for fetching students and teachers by id

--- src/main/java/com/schoolonline/app/user/UserController.java
+++ src/main/java/com/schoolonline/app/user/UserController.java
@@ -6,9 +6,11 @@ import com.schoolonline.app.user.dto.StudentDTO;
 import com.schoolonline.app.user.dto.TeacherDTO;
 import com.schoolonline.app.user.error.UserError;
 import io.vavr.control.Either;
+import io.vavr.control.Option;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -33,6 +35,18 @@ class UserController {
     ResponseEntity<?> addTeacher(@RequestBody NewUserDTO newUserDTO) {
         Either<UserError, TeacherDTO> teacherDTO = userFacade.addTeacher(newUserDTO);
         return ResponseProcessor.processResponse(teacherDTO, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/students/{studentId}")
+    ResponseEntity<?> getStudent(@PathVariable long studentId) {
+        Option<StudentDTO> student = userFacade.findStudentById(studentId);
+        return ResponseProcessor.processResponse(student, UserError.STUDENT_NOT_FOUND, HttpStatus.OK);
+    }
+
+    @GetMapping("/teachers/{teacherId}")
+    ResponseEntity<?> getTeacher(@PathVariable long teacherId) {
+        Option<TeacherDTO> teacher = userFacade.findTeacherById(teacherId);
+        return ResponseProcessor.processResponse(teacher, UserError.TEACHER_NOT_FOUND, HttpStatus.OK);
     }
 
     @DeleteMapping("/students/{studentId}")
