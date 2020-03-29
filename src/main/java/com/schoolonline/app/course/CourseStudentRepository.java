5b41355ffbcecf2267d29754a11da9ba23a9d8bc
ljurak <lucas.jurak@gmail.com>
2020-03-29 23:52:55 +0200

    Add query method

--- src/main/java/com/schoolonline/app/course/CourseStudentRepository.java
+++ src/main/java/com/schoolonline/app/course/CourseStudentRepository.java
@@ -1,8 +1,14 @@
 package com.schoolonline.app.course;
 
+import io.vavr.control.Option;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 interface CourseStudentRepository extends CrudRepository<CourseStudent, Long> {
+
+    @Query("select cs from CourseStudent cs where cs.studentId = :id and cs.course = :course")
+    Option<CourseStudent> findCourseStudentByStudentIdAndCourse(@Param("id") Long id, @Param("course") Course course);
 }
