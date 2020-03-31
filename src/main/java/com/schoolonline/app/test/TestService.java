58658a05ac1b25f376b42e809c3dca28fd705356
ljurak <lucas.jurak@gmail.com>
2020-03-31 04:33:38 +0200

    Add service for tests

---
+++ src/main/java/com/schoolonline/app/test/TestService.java
@@ -0,0 +1,24 @@
+package com.schoolonline.app.test;
+
+import com.schoolonline.app.test.dto.NewTestDTO;
+import com.schoolonline.app.test.dto.TestDTO;
+import com.schoolonline.app.test.error.TestError;
+import io.vavr.control.Either;
+
+class TestService {
+
+    private TestFactory testFactory;
+
+    private TestRepository testRepository;
+
+    public TestService(TestFactory testFactory, TestRepository testRepository) {
+        this.testFactory = testFactory;
+        this.testRepository = testRepository;
+    }
+
+    Either<TestError, TestDTO> addTest(NewTestDTO newTestDTO) {
+        return testFactory
+                .addTest(newTestDTO)
+                .map(Test::toDTO);
+    }
+}
