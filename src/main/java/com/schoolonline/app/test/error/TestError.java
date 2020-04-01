a6217596f6a3ccf273756242436d8084b0e6169b
ljurak <lucas.jurak@gmail.com>
2020-04-01 04:08:59 +0200

    Add errors for questions

--- src/main/java/com/schoolonline/app/test/error/TestError.java
+++ src/main/java/com/schoolonline/app/test/error/TestError.java
@@ -11,7 +11,13 @@ public enum TestError implements HttpResponseError {
     MISSING_ACTIVE_TO("Missing active to date", 400),
     INVALID_ACTIVE_TO("Invalid active to date", 400),
     MISSING_COURSE_ID("Missing course id", 400),
-    COURSE_NOT_FOUND("Course not found", 404);
+    COURSE_NOT_FOUND("Course not found", 404),
+    MISSING_DESCRIPTION("Missing description", 400),
+    INVALID_DESCRIPTION("Invalid description", 400),
+    MISSING_ANSWERS("Missing answers", 400),
+    INVALID_ANSWERS("Invalid answers", 400),
+    MISSING_CORRECT_ANSWER("Missing correct answer", 400),
+    TEST_NOT_FOUND("Test not found", 404);
 
     private String errorMessage;
 
