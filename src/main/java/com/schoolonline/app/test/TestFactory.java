d8ac6402ab3e43f525f44de696fb69f91dab2bf0
ljurak <lucas.jurak@gmail.com>
2020-04-01 04:14:56 +0200

    Add method for adding question for test

--- src/main/java/com/schoolonline/app/test/TestFactory.java
+++ src/main/java/com/schoolonline/app/test/TestFactory.java
@@ -1,8 +1,10 @@
 package com.schoolonline.app.test;
 
+import com.schoolonline.app.test.dto.NewQuestionDTO;
 import com.schoolonline.app.test.dto.NewTestDTO;
 import com.schoolonline.app.test.error.TestError;
 import io.vavr.control.Either;
+import io.vavr.control.Option;
 
 class TestFactory {
 
@@ -10,9 +12,12 @@ class TestFactory {
 
     private TestRepository testRepository;
 
-    public TestFactory(TestValidator testValidator, TestRepository testRepository) {
+    private QuestionRepository questionRepository;
+
+    TestFactory(TestValidator testValidator, TestRepository testRepository, QuestionRepository questionRepository) {
         this.testValidator = testValidator;
         this.testRepository = testRepository;
+        this.questionRepository = questionRepository;
     }
 
     Either<TestError, Test> addTest(NewTestDTO newTestDTO) {
@@ -21,6 +26,20 @@ class TestFactory {
                 .map(newTest -> {
                     Test test = Test.fromDTO(newTest);
                     return testRepository.save(test);
+                });
+    }
+
+    Either<TestError, Question> addQuestionToTest(NewQuestionDTO newQuestionDTO) {
+        return testValidator
+                .validateQuestion(newQuestionDTO)
+                .flatMap(newQuestion -> {
+                    Option<Test> test = testRepository.findTestById(newQuestion.getTestId());
+                    if (test.isEmpty()) {
+                        return Either.left(TestError.TEST_NOT_FOUND);
+                    }
+                    Question question = Question.of(newQuestion, test.get());
+                    questionRepository.save(question);
+                    return Either.right(question);
                 });
     }
 }
